28f0b342c69f1665ed55996e2aea8cd88a6f2e6f
DanielMorais06 <ni2006morais@gmail.com>
2024-05-14 15:59:27 +0200

    Atualizar

--- public/index.php
+++ public/index.php
@@ -5,27 +5,25 @@ use Slim\Psr7\Response;
 use Slim\Factory\AppFactory;
 use Psr\Http\Server\RequestHandlerInterface;
 use Psr\Http\Message\ResponseInterface ;
+use App\Application\Middleware\ExampleBeforeMiddleware;
 use Psr\Http\Message\ServerRequestInterface as RequestInterface;
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$beforeMiddleware = function(RequestInterface $request, RequestHandlerInterface $handler){
-	$response = $handler->handle($request);
-	$existingContent = (string) $response->getBody();
-
-	$response = new Response();
-	$response->getBody()->write('Before' . $existingContent);
-	return $response;
-};
 
 
 
-$app->add($beforeMiddleware);
 $app->addErrorMiddleware(true, true, false);
 
 // Corrigido a rota e a definição correta dos parâmetros
 $app->get('/hello/{name}', function(RequestInterface $request, ResponseInterface $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+})->add(new ExampleBeforeMiddleware());
+
+$app->get('/user/{name}', function(RequestInterface $request, ResponseInterface $response, array $args) {
     $name = $args['name'];
     $response->getBody()->write("Hello, $name");
     return $response;
