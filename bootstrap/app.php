ce19a7aa6135da5ed0f87af04c7f96c408d723a5
DanielMorais06 <ni2006morais@gmail.com>
2024-05-15 09:37:14 +0200

    Update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,10 @@ session_start();
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$user = new \App\Models\User;
+var_dump($user);
+die();
+
 $app = new Slim\App([
     'settings' => [
         'displayErrorDetails' => true,
