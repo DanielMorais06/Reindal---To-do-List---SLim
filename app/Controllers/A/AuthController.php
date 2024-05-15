2d1d8521c989e802e9bb77b2fcbb35f823238561
DanielMorais06 <ni2006morais@gmail.com>
2024-05-15 14:50:53 +0200

    Wfsdmoxic

---
+++ app/Controllers/A/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class AuthController extends Controller{
+
+    public function getSignUp($request, $response){
+
+    }
+
+    public function postSignUp(){
+
+    }
+}
