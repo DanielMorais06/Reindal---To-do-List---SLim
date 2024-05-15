9460452527941def0373572ecf3eb3112e32a160
DanielMorais06 <ni2006morais@gmail.com>
2024-05-15 17:27:50 +0200

    Login

--- app/Controllers/Auth/AuthController.php
+++ app/Controllers/Auth/AuthController.php
@@ -15,18 +15,22 @@ class AuthController extends Controller{
         $nome = $request->getParam('accountName');
         $email = $request->getParam('email');
         $palavrapasse = $request->getParam('password');
+        $erro = 0;
 
         if (preg_match('/[0-9]/', $nome)) {
             echo "O nome não pode conter números.";
+            $erro = 1;
         }
 
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             echo "O email não está em um formato válido.";
+            $erro =  1;
         }
 
         // Verificar se a senha atende aos critérios (letras maiúsculas, minúsculas e números)
         if (!preg_match('/[A-Z]/', $palavrapasse) || !preg_match('/[a-z]/', $palavrapasse) || !preg_match('/[0-9]/', $palavrapasse)) {
             echo "A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula e um número.";
+            $erro = 1;
         }
 
         // Consulta SQL para verificar se o email já está presente na tabela
@@ -40,9 +44,49 @@ class AuthController extends Controller{
             echo "O email já está registrado.";
         }
 
-        echo "Nome: $nome<br>";
-        echo "Email: $email<br>";
-        echo "Senha: $palavrapasse<br>";
+        if($erro == 0){
+            $sql = "INSERT INTO utilizadores (email, nome, palavrapasse) VALUES (:email, :nome, :palavrapasse)";
+            $stmt = $this->container->db->prepare($sql);
+
+            // Execute a consulta com os valores fornecidos
+            $stmt->execute(['email' => $email, 'nome' => $nome, 'palavrapasse' => $palavrapasse]);
+
+            // Verifica se a inserção foi bem-sucedida
+            if ($stmt->rowCount() > 0) {
+                return $this->container->view->render($response, 'index.html');
+            } else {
+                echo "Erro ao inserir registro.";
+            }
+        }
+
+
+        
+    }
+
+    public function getSignIn($request, $response){
+
+        return $this->container->view->render($response, 'auth/signin.html');
+
+    }
+
+    public function postSignIn($request, $response){
+        $email = $request->getParam('email');
+        $palavrapasse = $request->getParam('password');
+
+        $sql = "SELECT id_utilizador FROM utilizadores WHERE email=:email AND palavrapasse:palavrapasse";
+        $stmt = $this->container->db->prepare($sql);
+        $stmt->execute(['email' => $email, 'palavrapasse' => $palavrapasse]);
+
+        // Verifica se o email existe na base de dados
+        if ($stmt->rowCount() > 0) {
+            // Se o email existe, armazena o ID do usuário em uma variável
+            $id_utilizador = $stmt->fetchColumn();
+            $_SESSION['ID'] = $id_utilizador;
+            
+            return $response->withRedirect('../index.html');
+        } else {
+            echo "Email não encontrado na base de dados.";
+        }
 
     }
 }
