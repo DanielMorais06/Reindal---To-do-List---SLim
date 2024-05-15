eb8d2f4c066c6827db8a1d7fbba25e10bbd0c44d
DanielMorais06 <ni2006morais@gmail.com>
2024-05-15 15:21:15 +0200

    SignUp

--- app/Controllers/Auth/AuthController.php
+++ app/Controllers/Auth/AuthController.php
@@ -1,10 +1,13 @@
 <?php
 
-namespace App\Controllers;
+namespace App\Controllers\Auth;
+use App\Controllers\Controller;
 
 class AuthController extends Controller{
 
     public function getSignUp($request, $response){
+
+        return $this->container->view->render($response, 'auth/signup.php');
 
     }
 
