1cbdfda5a1a28f5951c4c7e0367f865b4d647d64
DanielMorais06 <ni2006morais@gmail.com>
2024-05-15 09:39:44 +0200

    Atualizar

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,10 +4,6 @@ session_start();
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$user = new \App\Models\User;
-var_dump($user);
-die();
-
 $app = new Slim\App([
     'settings' => [
         'displayErrorDetails' => true,
