2b8c84e0fa123e956cbcaee7e8efdb076366f6a8
DanielMorais06 <ni2006morais@gmail.com>
2024-05-15 10:38:36 +0200

    Update

--- app/routes.php
+++ app/routes.php
@@ -1,9 +1,1 @@
 <?php
-
-use Slim\App;
-use App\Controllers\TodoController;
-
-return function (App $app) {
-    $app->get('/todos', [TodoController::class, 'index']);
-    $app->post('/todos', [TodoController::class, 'create']);
-};
