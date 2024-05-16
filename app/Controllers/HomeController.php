ce4d68b1e2311d0d098974e6f97fe5d1b2162a8c
DanielMorais06 <ni2006morais@gmail.com>
2024-05-16 18:07:04 +0200

    Update

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -5,8 +5,15 @@ namespace App\Controllers;
 class HomeController extends Controller{
 
     public function index($request, $response){
-
-        return $this->container->view->render($response, '/sneat-1.0.0/html/index.html');
+        $query = "SELECT * FROM tasks";
+        $stmt = $this->container->db->query($query);
+        $tasks = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    
+        // Retorna os dados das tarefas em formato JSON
+        $tasksJson = json_encode($tasks);
+    
+        // Renderiza o HTML com os dados das tarefas
+        return $this->container->view->render($response, '/sneat-1.0.0/html/index.html', ['tasksJson' => $tasksJson]);
     }
-   
+    
 }
--- app/Controllers/TaskController.php
+++ app/Controllers/TaskController.php
@@ -11,4 +11,12 @@ class TaskController extends Controller{
     public function postNewTask($request, $response){
         
     }
+
+    public function getTasks($request, $response){
+        return $this->container->view->render($response, 'sneat-1.0.0/html/NewTask.html');
+    }
+
+    public function postTasks($request, $response){
+        
+    }
 }
