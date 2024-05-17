52ad3e8a08876ec1fc9e18ea237e6257cf65408d
DanielMorais06 <ni2006morais@gmail.com>
2024-05-17 16:09:26 +0200

    Update

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -9,17 +9,17 @@ class HomeController extends Controller{
             return $this->container->view->render($response, '/sneat-1.0.0/html/index.html');
         }else{
             $iduser = $_SESSION['Id_User'];
-            $query1 = "SELECT * FROM tasks WHERE category = '1' AND id_user = '$iduser' LIMIT 4";
+            $query1 = "SELECT * FROM tasks WHERE urgenza = '1' AND id_user = '$iduser' LIMIT 4";
         $stmt1 = $this->container->db->query($query1);
         $tasks1 = $stmt1->fetchAll(\PDO::FETCH_ASSOC);
         $tasksJson1 = json_encode($tasks1);
     
-        $query2 = "SELECT * FROM tasks WHERE category = '2' AND id_user = '$iduser' LIMIT 4";
+        $query2 = "SELECT * FROM tasks WHERE urgenza = '2' AND id_user = '$iduser' LIMIT 4";
         $stmt2 = $this->container->db->query($query2);
         $tasks2 = $stmt2->fetchAll(\PDO::FETCH_ASSOC);
         $tasksJson2 = json_encode($tasks2);
     
-        $query3 = "SELECT * FROM tasks WHERE category = '3' AND id_user = '$iduser' LIMIT 4"; 
+        $query3 = "SELECT * FROM tasks WHERE urgenza = '3' AND id_user = '$iduser' LIMIT 4"; 
         $stmt3 = $this->container->db->query($query3);
         $tasks3 = $stmt3->fetchAll(\PDO::FETCH_ASSOC);
         $tasksJson3 = json_encode($tasks3);
--- app/Controllers/TaskController.php
+++ app/Controllers/TaskController.php
@@ -10,31 +10,26 @@ class TaskController extends Controller{
             return $this->container->view->render($response, 'sneat-1.0.0/html/Login.html');
         }else{
             $iduser = $_SESSION['Id_User'];
-            $query1 = "SELECT * FROM tasks WHERE category = '1' AND id_user = '$iduser' LIMIT 4";
+            $query1 = "SELECT * FROM tasks WHERE urgenza = '1' AND id_user = '$iduser' LIMIT 4";
         $stmt1 = $this->container->db->query($query1);
         $tasks1 = $stmt1->fetchAll(\PDO::FETCH_ASSOC);
         $tasksJson1 = json_encode($tasks1);
     
-        $query2 = "SELECT * FROM tasks WHERE category = '2' AND id_user = '$iduser' LIMIT 4";
+        $query2 = "SELECT * FROM tasks WHERE urgenza = '2' AND id_user = '$iduser' LIMIT 4";
         $stmt2 = $this->container->db->query($query2);
         $tasks2 = $stmt2->fetchAll(\PDO::FETCH_ASSOC);
         $tasksJson2 = json_encode($tasks2);
     
-        $query3 = "SELECT * FROM tasks WHERE category = '3' AND id_user = '$iduser' LIMIT 4"; 
+        $query3 = "SELECT * FROM tasks WHERE urgenza = '3' AND id_user = '$iduser' LIMIT 4"; 
         $stmt3 = $this->container->db->query($query3);
         $tasks3 = $stmt3->fetchAll(\PDO::FETCH_ASSOC);
         $tasksJson3 = json_encode($tasks3);
 
-        $query4 = "SELECT * FROM tasks WHERE id_user = '$iduser'"; 
-        $stmt4 = $this->container->db->query($query4);
-        $tasks4 = $stmt4->fetchAll(\PDO::FETCH_ASSOC);
-        $tasksJson4 = json_encode($tasks4);
     
         return $this->container->view->render($response, '/sneat-1.0.0/html/NewTask.html', [
             'tasksJson1' => $tasksJson1,
             'tasksJson2' => $tasksJson2,
-            'tasksJson3' => $tasksJson3,
-            'tasksJson4' => $tasksJson4 
+            'tasksJson3' => $tasksJson3 
         ]);
         } 
     }
